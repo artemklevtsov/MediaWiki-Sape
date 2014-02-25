06a882b14933d6d01e0dc020813d685fb6a8678d
Artem Klevtsov <a.a.klevtsov@gmail.com>
2014-02-25 22:21:45 +0700

    Ads dont't appears on the page if ads block is empty.

--- Sape.body.php
+++ Sape.body.php
@@ -3,12 +3,13 @@
 class Sape {
     public static function wfSapeSidebar( Skin $skin, &$bar ) {
 	global $IP, $wgServer, $wgSapeUserID;
-	
+
 	// Return $bar unchanged if not all values have been set.
 	if ( $wgSapeUserID == 'none' ) {
 	    return $bar;
 	}
-	
+
+	// Return $bar for anonymous users only
 	if ( $skin->getUser()->isLoggedIn() ) {
 	    return $bar;
         }
@@ -16,18 +17,21 @@ class Sape {
        	define( '_SAPE_USER', $wgSapeUserID );
 
 	require_once( "$IP/$wgSapeUserID/sape.php" );
-	
+
 	$sape = new SAPE_client(array(
 	    'host' => $wgServer,
 	    'charset' => 'UTF-8'
 	));
 
 	$out = $sape->return_links();
-	
-	if ( strstr($out, 'href') !== FALSE ) {
-	    $out = '<ul><li>'.$out.'</li></ul>';
-	}
-	
+
+	// Return $bar if page haven't sape links.
+        if ( ! empty($out) ) {
+            $out = '<ul><li>'.$out.'</li></ul>';
+        } else {
+            return $bar;
+        }
+
 	$bar['sape-sidebar-heading'] = $out;
 	return true;
     }
